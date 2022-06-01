3cea82da1554c8d9d0475f431f59ad9935ba385d
Vinicius Araujo <vinicus.ar02@gmail.com>
2022-06-01 11:50:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>
+          Va
+        </h1>
+        <nav>
+          <span> about </span>
+          <span> Projects </span>
+          <span> contact </span>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
