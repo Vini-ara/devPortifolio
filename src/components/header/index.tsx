f84970bd4069d80c21c3e815b9e93aefa45dc454
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-06-17 17:30:43 -0300

    first version finished

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,21 @@
-import React, { useState, useEffect, useRef } from "react";
-import { HeaderWrapper } from './styles';
+import React, { useState } from "react";
+
 import { useScrollDirection } from '../../hooks/useScrollDirection'
+
+
+import { HeaderWrapper, HeaderModal } from './styles';
+import { BsList, BsX } from 'react-icons/bs';
+
+interface handleScrollProps {
+  prev: number;
+  curr: number;
+}
 
 export const Header: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
+  const [isModalActive, setIsModalActive] = useState(false);
 
-  function handleScroll({prev, curr}) {
+  function handleScroll({prev, curr}: handleScrollProps) {
     if(prev > curr || curr < 80) {
       setIsVisible(true)
       return
@@ -24,11 +34,22 @@ export const Header: React.FC = () => {
         <h1>
           vini.araujo()
         </h1>
-        <nav>
-          <a href="#about"> About </a>
-          <a href="#projects"> Projects </a>
-          <a href="#contact"> Contact </a>
-        </nav>
+        <div>
+          <BsList onClick={() => { if(!isModalActive) setIsModalActive(!isModalActive) }}/> 
+          <nav>
+            <a href="#about"> &lt;About/&gt; </a>
+            <a href="#projects"> &lt;Projects/&gt; </a>
+            <a href="#contact"> &lt;Contact/&gt; </a>
+          </nav>
+        </div>
+        <HeaderModal isActive={isModalActive} >
+          <BsX onClick={() => setIsModalActive(!isModalActive)}/>
+          <nav>
+            <a href="#about" onClick={() => setIsModalActive(!isModalActive)}> &lt;About/&gt; </a>
+            <a href="#projects" onClick={() => setIsModalActive(!isModalActive)}> &lt;Projects/&gt; </a>
+            <a href="#contact" onClick={() => setIsModalActive(!isModalActive)}> &lt;Contact/&gt; </a>
+          </nav>
+        </HeaderModal>
     </HeaderWrapper>
   )
 }
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,7 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface HeaderWrapperProps {
   isVisible: boolean;
+}
+
+interface HeaderModalProps {
+  isActive: boolean;
 }
 
 export const HeaderWrapper = styled.header<HeaderWrapperProps>`
@@ -28,6 +32,22 @@ export const HeaderWrapper = styled.header<HeaderWrapperProps>`
     font-size: 2.25rem;
   }
 
+  div {
+    display: flex;
+
+    svg {
+      color: ${props => props.theme.text};
+      display: none;
+      width: 0; height: 0; 
+      visibility: hidden; 
+      opacity: 0;
+
+      &:hover {
+        color: ${props => props.theme.high_text} 
+      }
+    }
+  }
+
   nav {
     a {
       font-size: 1.5rem;  
@@ -44,5 +64,94 @@ export const HeaderWrapper = styled.header<HeaderWrapperProps>`
 
   @media (max-width: 1024px) {
     padding: 0 5rem; 
+  }
+
+  @media (max-width: 768px) {
+    nav {
+      display: none;
+      width: 0; height: 0; 
+      visibility: hidden; 
+    }
+
+    div {
+      svg {
+        display: block;
+        width: 2rem; height: 2rem; 
+        visibility: visible; 
+        opacity: 1;
+      }
+    }
+  }
+
+  @media (max-width: 650px) {
+    h1 {
+      font-size: 1.75rem;
+    }
+
+    nav {
+      a {
+        font-size: 1.25rem;
+        margin-left: .4rem;
+      }
+    }
+  }
+
+  @media (max-width: 500px) {
+    padding: 0 2.5rem;
+
+    h1 {
+      font-size: 1.5rem;
+    }
+  }
+`
+export const HeaderModal = styled.div<HeaderModalProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  ${props => props.isActive ? css`
+    display: flex; 
+    visibility: visible;
+    opacity: 1;
+    z-index: 99;
+    height: 100vh; width: 100vw;  
+  ` : css`
+    display: none; 
+    visibility: hidden;
+    opacity: 0;
+    z-index: -99;
+    height: 0; width: 0;  
+  `}
+
+  background: ${props => props.theme.bg};
+
+  svg {
+    position: fixed;
+    top: 1.5rem;
+    right: 2.5rem;
+  }
+
+  nav {
+    width: 100%; height: fit-content; 
+    visibility: visible; 
+    margin-top: 20vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    gap: 2rem;
+
+    a {
+      font-size: 2rem;  
+      margin-left: 1rem;    
+      cursor: pointer;
+      text-decoration: none;
+      color: ${props => props.theme.text};
+
+      &:hover {
+        color: ${props => props.theme.high_text} 
+      }
+    }
   }
 `
